7621b07c8250e27ac6027394c0cf9359835bfc1a
ishaanlodha <ishaanlodha@gmail.com>
2019-11-11 07:40:10 +0530

    Calendar dependencies

---
+++ cal_api_dep.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install python3-pip
+pip3 install flask
+pip install Flask-PyMongo
+pip3 install Flask-Cors
---
+++ cal_react_dep.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs
+npm install -g create-react-app
+npm install --save bootstrap
+npm install --save reactstrap react react-dom
